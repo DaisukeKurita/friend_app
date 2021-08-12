1683a217d597cc6dae1fa7f6673170ef0c2d541f
栗田大輔 <daikons.msn36@gmail.com>
2021-08-12 16:26:15 +0900

    テーブルのtitleカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_055914) do
+ActiveRecord::Schema.define(version: 2021_08_12_072455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "feeds", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.text "image"
     t.datetime "created_at", null: false
