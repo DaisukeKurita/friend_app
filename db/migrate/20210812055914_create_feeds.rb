9e4c269ae0eeeafa392a0b6923dc9f4bfa136985
栗田大輔 <daikons.msn36@gmail.com>
2021-08-12 16:18:14 +0900

    テーブルのカラムを変更

--- db/migrate/20210812055914_create_feeds.rb
+++ db/migrate/20210812055914_create_feeds.rb
@@ -1,6 +1,8 @@
 class CreateFeeds < ActiveRecord::Migration[5.2]
   def change
     create_table :feeds do |t|
+      t.string :title
+      t.text :content
       t.text :image
 
       t.timestamps
