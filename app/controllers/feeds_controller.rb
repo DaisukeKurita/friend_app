240b3ad81f9ef27647293f0f5223cc39e313782e
栗田大輔 <daikons.msn36@gmail.com>
2021-08-12 16:51:20 +0900

    feeds/new.html.erbで文章が書けるようにした

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,15 @@
 class FeedsController < ApplicationController
   def new
+    @feed = Feed.new
+  end
+
+  def create
+    Feed.create(feed_params)
+    redirect_to new_feed_path
+  end
+
+  private
+  def feed_params
+    params.require(:feed).permit(:content, :image, :image_cache)
   end
 end
